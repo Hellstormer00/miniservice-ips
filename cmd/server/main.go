0f30a402abf335694ce4a4e1334e7f82554b3727
Alexander Haas <alexander-christian.haas@siemens.com>
2021-12-14 19:02:03 +0100

    added the ipholder struct

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/hellstormer00/miniservice-ips/pkg"
 )
 
+const (
+	bufsize = 50000
+)
+
+type IpHolder struct {
+	mu  sync.Mutex
+	ips []string
+}
+
+func (holder *IpHolder) AddIp(new_ip string) {
+	holder.mu.Lock()
+
+	result := false
+	for _, ip := range holder.ips {
+		if new_ip == ip {
+			result = true
+			break
+		}
+	}
+
+	if result {
+		holder.ips = append(holder.ips)
+	}
+
+	holder.mu.Unlock()
+}
+
 func main() {
-	http.HandleFunc("/logs", pkg.HandleLogRequest)
+	ch := make(chan string, 10)
+	ipHolder := IpHolder{
+		ips: make([]string, bufsize),
+	}
+	http.HandleFunc("/logs", pkg.HandleLogRequest(ch))
 	http.HandleFunc("/visitors", pkg.HandleVisitorRequest)
 	http.ListenAndServe(":5000", nil)
 }
