9309986b21af19aa5f275750a4732fdc427b3492
Alexander Haas <alexander-christian.haas@siemens.com>
2021-12-14 19:02:44 +0100

    decoding json and error handling in /logs

--- pkg/Handlers.go
+++ pkg/Handlers.go
@@ -1,14 +1,44 @@
 package pkg
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
-func HandleLogRequest(w http.ResponseWriter, r *http.Request) {
-	log.Println("New request:", r.Method, r.URL)
+type logRequest struct {
+	TimeStamp string `json:"timestamp"`
+	Ip        string `json:"ip"`
+	Url       string `json:"url"`
+}
+
+func HandleLogRequest(ch chan string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("New request:", r.Method, r.URL)
+
+		if r.Method != http.MethodPost {
+			http.Error(w, "This API only uses POST", 400)
+		}
+
+		if r.Body == nil {
+			http.Error(w, "No request body provided", 400)
+		}
+
+		var req logRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+		}
+
+	}
 }
 
 func HandleVisitorRequest(w http.ResponseWriter, r *http.Request) {
 	log.Println("New request:", r.Method, r.URL)
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "This API only uses GET", 400)
+	}
+
 }
